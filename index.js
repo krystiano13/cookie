0c2d0e62037d26d7dc2cf1ebf109b3c7aa8d347b
krystiano13 <crystianotv@gmail.com>
2024-02-25 17:55:21 +0100

    create function for deleting cookie

--- index.js
+++ index.js
@@ -5,8 +5,22 @@
  * @param { number } daysToExpire
  */
 export function setCookie(name, value, daysToExpire) {
+    /** @type Date */
     const date = new Date();
     date.setTime(date.getTime() + daysToExpire * 24 * 60 * 60 * 1000);
+    /** @type string */
     const expire = `expires=${date.toUTCString()}`;
     document.cookie = `${name}=${value}; ${expire}; path=/`;
+}
+
+/**
+ *
+ * @param name
+ */
+export function deleteCookie(name) {
+    document.cookie = `${name}=${null}; ${null}; path=/`
+}
+
+export function getCookie() {
+
 }
